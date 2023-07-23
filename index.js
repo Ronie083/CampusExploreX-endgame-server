c1b84559c43c81bd00b0b6963cdd8d869931b9e3
Ronie083 <ahmedronie6@gmail.com>
2023-07-24 03:46:39 +0600

    2nd commit

--- index.js
+++ index.js
@@ -14,43 +14,66 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const universitiesColl = client.db("campusExploreDb").collection("universities");
-    const reviewsColl = client.db("campusExploreDb").collection("reviews");
+        const universitiesColl = client.db("campusExploreDb").collection("universities");
+        const reviewsColl = client.db("campusExploreDb").collection("reviews");
+        const appliedColl = client.db("campusExploreDb").collection("appliedIn");
 
-    app.get('/universities', async(req, res) =>{
-        const result = await universitiesColl.find().toArray();
-        res.send(result);
-    })
+        app.get('/universities', async (req, res) => {
+            const result = await universitiesColl.find().toArray();
+            res.send(result);
+        })
 
-    app.get('/reviews', async(req, res) =>{
-        const result = await reviewsColl.find().toArray();
-        res.send(result);
-    })
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewsColl.find().toArray();
+            res.send(result);
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.get('/universities/:id', async (req, res) => {
+            const id = parseInt(req.params.id);
+            const query = { "id": id };
+            const options = {
+                projection: { id: 0 },
+            };
+            const result = await universitiesColl.findOne(query, options);
+            res.send(result);
+        });
+
+
+        app.post('/applied', async (req, res) => {
+            const studentData = req.body;
+            const result = await appliedColl.insertOne(studentData);
+            res.send(result)
+        })
+
+        app.get('/applied', async (req, res) => {
+            const result = await appliedColl.find().toArray();
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('Server of Campus Explore X is here')
 })
 
